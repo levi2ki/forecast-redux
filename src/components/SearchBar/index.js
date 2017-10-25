780adb5f4c5acc2fb1f358bb3f183cb6a62d63d8
levi2ki <levi2ki1988@gmail.com>
2017-10-25 22:02:22 +0300

    fixed broken link code-style format

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,14 +4,14 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {term: ''};
+        this.state = { term: '' };
     }
 
-    handleChange = (event) => this.setState({term: event.target.value});
+    handleChange = (event) => this.setState({ term: event.target.value });
     onFromSubmit = (event) => {
         event.preventDefault();
         this.props.fetchWeather(this.state.term);
-        this.setState({term: ''});
+        this.setState({ term: '' });
     };
 
     render() {
--- src/containers/SearchBarContainer/index.js
+++ src/containers/SearchBarContainer/index.js
@@ -7,6 +7,6 @@ import SearchBar from '../../components/SearchBar';
 
 
 const mapStateToProps = (state) => state;
-const mapDispatchToProps = (dispatch) => bindActionCreators({fetchWeather}, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ fetchWeather }, dispatch);
 
 export default connect(null, mapDispatchToProps)(SearchBar);
